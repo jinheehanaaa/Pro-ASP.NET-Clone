d512e68493d87c59bdfe8a82188fc75936e5f3fa
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-09-15 16:54:36 -0500

    | Dynamic Output "Good Morning" or "Good Afternoon" |

--- FirstProject/Controllers/HomeController.cs
+++ FirstProject/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace FirstProject.Controllers
 {
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            return View("MyView");
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", viewModel);
         }
     }
 }
