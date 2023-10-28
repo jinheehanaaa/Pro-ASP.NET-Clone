72fccff9429b40b2bafdc4fae56932b2ff69627c
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-10-28 11:41:18 -0500

    | Index action method for rendering an array of strings |

---
+++ LanguageFeatures/Startup.cs
@@ -0,0 +1,30 @@
+﻿
+// Enabling the MVC Framework (Default Configuration)
+namespace LanguageFeatures
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapGet("/", async context => {
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
